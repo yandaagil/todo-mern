61c74e8033e7e8cc8e3945f73b569fcb01e1a5fa
Yanda Agil <yandaagil@gmail.com>
2024-10-23 22:02:06 +0700

    chore: update next.config.js to include additional transpile packages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 const nextConfig = {
   /* config options here */
   reactStrictMode: true,
+  transpilePackages: [
+    "rc-util",
+    "@ant-design/icons-svg",
+    "@ant-design/icons",
+    "antd",
+    "@ant-design/cssinjs",
+    "@ant-design/react-slick",
+    "rc-pagination",
+    "rc-picker",
+    "rc-util",
+    "rc-checkbox",
+    "rc-dialog",
+  ],
 };
 
 module.exports = nextConfig;
